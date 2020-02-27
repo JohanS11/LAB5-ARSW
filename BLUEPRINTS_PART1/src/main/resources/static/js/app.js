726198f51b0edea13cd7d0b14c200b60a3b484fd
unknown <andres.marcelo@labinfo.is.escuelaing.edu.co>
2020-02-27 10:03:29 -0500

    Avance

---
+++ BLUEPRINTS_PART1/src/main/resources/static/js/app.js
@@ -0,0 +1,9 @@
+$("#bttnSubmit").click(function(){
+    console.log("a");
+    var aut = $("#nameValue").val();
+    path="/blueprints/"+ aut;
+    var URLactual = window.location;
+    console.log(URLactual+path);
+});
+
+
