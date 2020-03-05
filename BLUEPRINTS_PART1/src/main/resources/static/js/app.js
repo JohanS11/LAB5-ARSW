fc925b91d6e5a9def6b7ae78d54e5ce48343dc3c
Andresmarcelo7 <felipemarcelo156@gmail.com>
2020-03-05 09:36:44 -0500

    LAB06 funcionalidad dibujar y adicion botones

--- BLUEPRINTS_PART1/src/main/resources/static/js/app.js
+++ BLUEPRINTS_PART1/src/main/resources/static/js/app.js
@@ -1,4 +1,5 @@
 var services  = (function(){
+    var selectedBlueprint = "";
     function doMap(blueprints) {
         return blueprints.map( function(bp){
             return{
@@ -29,30 +30,69 @@ var services  = (function(){
     }
 
     function dibujar(blueprint){
-        console.log(blueprint);
         $("#currentBlueprint").text("The current Blueprint is: " + blueprint.name)
         var can = document.getElementById("myCanvas");
         var lapiz = can.getContext("2d");
         lapiz.clearRect(0,0,can.width,can.height);
         lapiz.beginPath();
         var punto = blueprint.points[0];
-
+        console.log(punto);
         lapiz.moveTo(punto.x,punto.y);
         blueprint.points.map(function(p){
             lapiz.lineTo(p.x,p.y)
             },1
         )
         lapiz.stroke();
+        selectedBlueprint=blueprint;
+                if(window.PointerEvent) {
+
+                  can.addEventListener("pointerdown", continueDraw, false);
+                }
+                else {
+                  //Provide fallback for user agents that do not support Pointer Events
+                  can.addEventListener("mousedown", continueDraw, false);
+                }
+    }
+
+    function continueDraw(event) {
+        var canvas = document.getElementById('myCanvas');
+            var offset  = getOffset(canvas);
+            if (canvas.getContext) {
+              var ctx = canvas.getContext("2d");
+              ctx.lineTo(ctx.pageX,ctx.pageY)
+              ctx.stroke();
+           ctx.fillStyle = '#ff0000';
+           ctx.fillRect(event.pageX-offset.left, event.pageY-offset.top, 5, 5);
+           selectedBlueprint.points.push({x:event.pageX-offset.left, y:event.pageY-offset.top});
+           ctx.lineTo(event.pageX-offset.left,event.pageY-offset.top);
+           ctx.stroke();
+           console.log(selectedBlueprint)
+           }
 
     }
+
+    function getOffset(obj) {
+        var offsetLeft = 0;
+        var offsetTop = 0;
+              do {
+                if (!isNaN(obj.offsetLeft)) {
+                    offsetLeft += obj.offsetLeft;
+                }
+                if (!isNaN(obj.offsetTop)) {
+                    offsetTop += obj.offsetTop;
+                }
+              } while(obj = obj.offsetParent );
+              return {left: offsetLeft, top: offsetTop};
+          }
 
     function funcione(){
         var aut = $("#nameValue").val();
         $("#authorLabel").text(aut+"'s blueprints:");
-        apimock.getBlueprintsByAuthor(aut,llenaTabla)
+        apidata.getBlueprintsByAuthor(aut,llenaTabla)
     }
     function searchAuthorByName(name,author) {
-        apimock.getBlueprintsByNameAndAuthor(name,author,dibujar)
+
+        selectedBlueprint = apidata.getBlueprintsByNameAndAuthor(name,author,dibujar)
     }
     return {
         funcione:funcione,
